b43476f5fc2a99ef29029e600fa6cf27d0bdc5e4
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-02-09 17:09:55 +0100

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,16 @@
+import os, pandas, csv, re
+import numpy as np
+from biothings.utils.dataload import dict_convert, dict_sweep
+from biothings import config
+logging = config.logger
+def load_dbSNP(data_folder):
+    infile = os.path.abspath("/opt/biothings/GRCh37/dbSNP/b151/Dbsnp.tsv")
+    assert os.path.exists(infile)
+    dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
+    results = {}
+    for rec in dat:
+        _id = rec["release"] + "_" + str(rec["chromosome"]) + "_" + str(rec["position"]) + "_" + rec["reference"] + "_" + rec["alternative"]       
+		process_key = lambda k: k.replace(" ","_").lower()\nrec = dict_convert(rec,keyfn=process_key)\nresults.setdefault(_id,[]).append(rec)
+    for _id,docs in results.items():
+        doc = {"_id": _id, "dbSNP" : docs}
+        yield doc
